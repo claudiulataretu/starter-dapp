40a7dbfee775c633108e7e7941539d0cddf88713
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-03-20 10:59:34 +0200

    add functionality for multiple content
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- react-delegationdashboard/src/components/LandingPage/ContentBlock/LeftContentBlock/index.tsx
+++ react-delegationdashboard/src/components/LandingPage/ContentBlock/LeftContentBlock/index.tsx
@@ -1,19 +1,23 @@
 import { Row, Col } from 'antd';
 import { Slide } from 'react-awesome-reveal';
+import { SectionDetails } from '../../../../helpers/types';
 
 import SvgIcon from '../../../../common/SvgIcon';
+import Button from '../../../../common/Button';
+import { ButtonDetails } from '../../../../helpers/types';
 
 import * as S from './styles';
 
 interface LeftContentBlockType {
   icon?: string;
   title?: string,
-  content?: string;
-  section?: object[];
+  content?: string[];
+  section?: SectionDetails[];
+  button?: ButtonDetails[];
   id?: string;
 }
 
-const LeftContentBlock = ({ icon, title, content, section, id }: LeftContentBlockType) => {
+const LeftContentBlock = ({ icon, title, content, section, button, id }: LeftContentBlockType) => {
   return (
     <S.LeftContentBlock>
       <Row justify='space-between' align="middle" id={id} style={{display:'flex'}}>
@@ -31,22 +35,46 @@ const LeftContentBlock = ({ icon, title, content, section, id }: LeftContentBloc
           <Slide direction='right'>
             <S.ContentWrapper>
               <h6>{title}</h6>
-              <S.Content>{content}</S.Content>
-              <S.ServiceWrapper>
+              <S.Content>
                 <Row justify="space-between">
-                  {/* {section &&
-                    typeof section === 'object' &&
+                  {content &&
+                  content.map((item, id) => {
+                    return (
+                      <p>
+                        {item}
+                      </p>
+                    );
+                  })}
+                </Row>
+                  {section &&
                     section.map((item, id) => {
                       return (
-                        <Col key={id} span={11}>
-                          <SvgIcon src={item.icon} width="60px" height="60px" />
-                          <S.MinTitle>{item.title}</S.MinTitle>
-                          <S.MinPara>{item.content}</S.MinPara>
+                        <Row justify="start" style={{display:'flex'}}>
+                        <Col span={2}>
+                          <SvgIcon src={item.icon} width="20px" height="20px" />
                         </Col>
+                        <Col span={22}>{item.content}</Col>
+                        </Row>
                       );
-                    })} */}
-                </Row>
-              </S.ServiceWrapper>
+                    })}
+                </S.Content>
+                <S.ButtonWrapper>
+                {button &&
+                  button.map((item, id) => {
+                    return (
+                      <Button
+                        key={id}
+                        color={item.color}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if (item.link) window.location.href=item.link;
+                        }}
+                      >
+                        {item.title}
+                      </Button>
+                    );
+                  })}
+              </S.ButtonWrapper>
             </S.ContentWrapper>
           </Slide>
         </Col>
